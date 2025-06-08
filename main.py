0513ce3366928d42ddfb990afb030ffa4a0461f6
JJ Zabkar <jjzabkar@gmail.com>
2025-06-08 12:16:39 -0700

    retain in memory history

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import uuid
+
 from dotenv import load_dotenv
 from typing import Annotated, Literal
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
 from langchain.chat_models import init_chat_model
+from langchain_core.chat_history import InMemoryChatMessageHistory
+
 from pydantic import BaseModel, Field
 from typing_extensions import TypedDict
 
@@ -49,10 +53,17 @@ def router(state: State):
 
     return {"next": "logical"}
 
+def get_message_content_with_context(state: State) -> str:
+    last_message = state["messages"][-1]
+    previous_messages = "\n".join(
+        message.content for message in reversed(state['messages'][0:-1])
+    )
+    content = f'{last_message}'
+    if previous_messages is not None and len(previous_messages) > 2:
+        content = content + f'\nPrevious contextual messages, ordered by recent first: \n{previous_messages}'
+    return content
 
 def therapist_agent(state: State):
-    last_message = state["messages"][-1]
-
     messages = [
         {"role": "system",
          "content": """You are a compassionate therapist. Focus on the emotional aspects of the user's message.
@@ -62,7 +73,7 @@ def therapist_agent(state: State):
          },
         {
             "role": "user",
-            "content": last_message.content
+            "content": get_message_content_with_context(state), # last_message.content
         }
     ]
     reply = llm.invoke(messages)
@@ -70,7 +81,6 @@ def therapist_agent(state: State):
 
 
 def logical_agent(state: State):
-    last_message = state["messages"][-1]
 
     messages = [
         {"role": "system",
@@ -81,15 +91,23 @@ def logical_agent(state: State):
          },
         {
             "role": "user",
-            "content": last_message.content
+            "content": get_message_content_with_context(state)
         }
     ]
     reply = llm.invoke(messages)
     return {"messages": [{"role": "assistant", "content": reply.content}]}
 
 
-graph_builder = StateGraph(State)
+chats_by_session_id = {}
+def get_chat_history(session_id: str) -> InMemoryChatMessageHistory:
+    chat_history = chats_by_session_id.get(session_id)
+    if chat_history is None:
+        chat_history = InMemoryChatMessageHistory()
+        chats_by_session_id[session_id] = chat_history
+    return chat_history
 
+
+graph_builder = StateGraph(State)
 graph_builder.add_node("classifier", classify_message)
 graph_builder.add_node("router", router)
 graph_builder.add_node("therapist", therapist_agent)
@@ -130,6 +148,9 @@ def run_chatbot_syncrhronous():
             print(f"Assistant: {last_message.content}")
 
 def run_chatbot_streaming():
+    session_id = uuid.uuid4()
+    config = {"configurable": {"session_id": session_id}}
+
     inputs = {"messages": [], "message_type": None}
     while True:
         user_input = input("Message: ")
@@ -138,8 +159,28 @@ def run_chatbot_streaming():
             break
         inputs['messages'].append(user_input)
         # https://langchain-ai.lang.chat/langgraph/how-tos/streaming/#streaming-api
-        for chunk in graph.stream(inputs, stream_mode="messages"):
+        for chunk in graph.stream(inputs, config, stream_mode="messages"):
             print(chunk[0].content, end="", flush=True)
+
+
+def run_chatbot_streaming_memory(state, config) -> list:
+    # Make sure that config is populated with the session id
+    if "configurable" not in config or "session_id" not in config["configurable"]:
+        raise ValueError(
+            "Make sure that the config includes the following information: {'configurable': {'session_id': 'some_value'}}"
+        )
+    # Fetch the history of messages and append to it any new messages.
+    chat_history = get_chat_history(config["configurable"]["session_id"])
+    messages = list(chat_history.messages) + state["messages"]
+
+    run_chatbot_syncrhronous(chat_history)
+
+    # Finally, update the chat message history to include
+    # the new input message from the user together with the
+    # response from the model.
+    chat_history.add_messages(state["messages"] + [ai_message])
+    # return {"messages": ai_message}
+
 
 if __name__ == "__main__":
     run_chatbot_streaming()
